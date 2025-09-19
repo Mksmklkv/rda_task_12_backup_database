fc36492e7f7a777dbe37d26583b6cfc264ff5f8b
Maksym Kulakov <maksym.kulakov@ica.de>
2025-09-19 23:03:21 +0200

    Task done

--- task.sh
+++ task.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+
+# full backup
+mysqldump -u "$DB_USER" -p "$DB_PASSWORD" --databases ShopDB  --result-file=backup.full.sql --no-create-db
+# restore from full backup
+mysql -u "$USER" -p "$DB_PASSWORD" ShopDBDevelopment < backup.full.sql  
+
+# data backup
+mysqldump -u "$USER" -p "$DB_PASSWORD" --databases ShopDB --result-file=backup.data.sql --no-create-db --no-create-info;
+# restore from data backup
+mysql -u "$USER" -p "$DB_PASSWORD" ShopDBDevelopment < backup.data.sql
+
