64a869fa1bf7a4811907a21bae97eb90c6d04215
Maksym Kulakov <maksym.kulakov@ica.de>
2025-09-21 18:18:51 +0200

    Task fixed

--- task.sh
+++ task.sh
@@ -16,12 +16,10 @@ check_var "DB_PASSWORD"
 echo "All variables exist..."
 
 # full backup
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB  --result-file=backup.full.sql --no-create-db
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --single-transaction --quick ShopDB  --result-file=backup.full.sql --no-create-db
 # restore from full backup
-mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < backup.full.sql 
-
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < backup.full.sql
 # data backup
-mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --databases ShopDB --result-file=backup.data.sql --no-create-db --no-create-info
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --single-transaction --quick ShopDB --result-file=backup.data.sql --no-create-db --no-create-info --skip-triggers
 # restore from data backup
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < backup.data.sql
-
